d4a330bd080f63d5a8e895d13bf754a521b85df9
Лаптев Артем <artemlaptev63@mail.ru>
2018-05-12 14:47:01 +0400

    test-swipe

--- src/ViewPhotos.js
+++ src/ViewPhotos.js
@@ -1,39 +1,46 @@
 import React, { Component } from 'react';
 import posts from './posts';
 import './ViewPhotos.css';
-import $ from "jquery";
-
-class ViewPhotos extends Component {
-    
-    render() {
-        $('#showAllPhotos img').on('swipeleft', function() {
-            this.props.nextPhoto();
-            console.log('next');
-        });
-        $('#showAllPhotos img').on('swiperight', function() {
-            this.props.prevPhoto();
-            console.log('prev');
-        });
-        // получаем фото на которое кликаем, в метод передаем id поста и номер фото
-        var image = posts.getImage(this.props.itemId, this.props.numberOfPhoto);
-
-        // в зависимости от состояния меняем стиль 'display: hidden' and 'display: block'
-        var show = {
-            display: this.props.display
-        };
-        
-        
-        return (
-            <div id='showAllPhotos' style={show}>
-                <div className='container'>
-                    <img onClick={this.props.nextPhoto} src={image} alt="" />
-                </div>
-                <div className="closeGallery" onClick={this.props.closeGallery}>&#215;</div>
-                <p onClick={this.props.prevPhoto} className='photoNavigationPrev'>&#60;</p>
-                <p onClick={this.props.nextPhoto} className='photoNavigationNext'>&#62;</p>
-            </div>
-        );
+window.addEventListener('load', function () {
+  var startX, startY, endX, endY, swipearea;
+  swipearea = document.getElementById('showAllPhotos')
+  swipearea.addEventListener('touchstart', function (e) {
+    startX = e.changedTouches[0].clientX
+    startY = e.changedTouches[0].clientY
+  }, false)
+  swipearea.addEventListener('touchend', function (e) {
+    endX = e.changedTouches[0].clientX
+    endY = e.changedTouches[0].clientY
+    if (endX - startX >= 30 && (Math.abs(endY - startY) <= 200)) {
+      console.log('hi');
     }
+  }, false)
+}, false)
+class ViewPhotos extends Component {
+
+
+
+  render() {
+    // получаем фото на которое кликаем, в метод передаем id поста и номер фото
+    var image = posts.getImage(this.props.itemId, this.props.numberOfPhoto);
+
+    // в зависимости от состояния меняем стиль 'display: hidden' and 'display: block'
+    var show = {
+      display: this.props.display
+    };
+
+
+    return (
+      <div id='showAllPhotos' style={show}>
+        <div className='container'>
+          <img onClick={this.props.nextPhoto} src={image} alt="" />
+        </div>
+        <div className="closeGallery" onClick={this.props.closeGallery}>&#215;</div>
+        <p onClick={this.props.prevPhoto} className='photoNavigationPrev'>&#60;</p>
+        <p onClick={this.props.nextPhoto} className='photoNavigationNext'>&#62;</p>
+      </div>
+    );
+  }
 }
 
 export default ViewPhotos;
