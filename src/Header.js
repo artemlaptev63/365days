428d696785bbb91cf7ecd79bcd8a4735beb966b3
Лаптев Артем <artemlaptev63@mail.ru>
2018-05-16 10:09:57 +0400

    Added MailChimp

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
+import SimpleForm from './MailchimpForm';
 
 class Header extends Component {
   render() {
@@ -17,25 +18,7 @@ class Header extends Component {
           <input type='email' placeholder="Enter Your Email" />
           <button>SUBSCRIBE</button>
           </form> */}
-          <div id="mc_embed_signup">
-            <form action="https://gmail.us9.list-manage.com/subscribe/post?u=f781c1fc1510a1ae4a7684179&amp;id=d3e2eebd35" method="post"
-              id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
-              <div id="mc_embed_signup_scroll">
-                <label htmlFor="mce-EMAIL">Subscribe to our mailing list</label>
-                <input type="email" value="" name="EMAIL" className="email" id="mce-EMAIL" placeholder="email address" required />
-                {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
-                <div style={{
-                  position: 'absolute',
-                  left: -5000 + 'px'
-                }} aria-hidden="true">
-                  <input type="text" name="b_f781c1fc1510a1ae4a7684179_d3e2eebd35" tabIndex="-1" value="" />
-                </div>
-                <div className="clear">
-                  <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" />
-                </div>
-              </div>
-            </form>
-          </div>
+          <SimpleForm />
         </div>
       </div>
     );
