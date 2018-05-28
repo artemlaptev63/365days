6faf4be3b8aecba1ed2ec502f990687e7cd73756
Лаптев Артем <artemlaptev63@mail.ru>
2018-05-28 20:19:20 +0400

    Added content in posts.js

--- src/posts.js
+++ src/posts.js
@@ -8,7 +8,7 @@ export default {
             years: '1908-2004',
             description: 'Henri Cartier-Bresson was a French humanist photographer considered a master of candid photography, and an early user of 35 mm film. He pioneered the genre of street photography, and viewed photography as capturing a decisive moment. His work has influenced many photographers. Cartier-Bresson acquired the Leica camera with 50 mm lens in Marseilles that would accompany him for many years. The anonymity that the small camera gave him in a crowd or during an intimate moment was essential in overcoming the formal and unnatural behavior of those who were aware of being photographed. He enhanced his anonymity by painting all shiny parts of the Leica with black paint. The Leica opened up new possibilities in photography—the ability to capture the world in its actual state of movement and transformation. Restless, he photographed in Berlin, Brussels, Warsaw, Prague, Budapest and Madrid.',
             autor: 'Henri Cartier-Bresson',
-            quote: 'The photograph itself doesn’t interest me. I want only to capture a minute part of reality',
+            quote: 'The photograph itself doesn’t interest me. I want only to capture a minute part of reality.',
             mainBannerTitle: '', //перед названием нужна запятая
             mainImage: '../assets/post1/albert-camus-paris-1944.jpg',
             image1: 'post1/alicante-spain-1932.jpg',
@@ -21,7 +21,7 @@ export default {
                 'post1/football-game-1960.jpg',
                 'post1/gahndi-1948.jpg',
                 'post1/gare-saint-lazare-1932.jpg',
-                'post1/hamburg-1952-1953. .jpg',
+                'post1/hamburg-1952-1953.jpg',
                 'post1/hyères-1932.jpg',
                 'post1/jean-paul-sartre-1946..jpg',
                 'post1/lenin-tomb-1954.jpg',
@@ -33,6 +33,94 @@ export default {
                 'post1/romania-1975.jpg',
                 'post1/simone-de-beauvoir-1946.jpg',
                 'post1/truman-capote-new-orleans-1947.jpg'
+            ]
+        },
+        {
+            id: 2,
+            country: 'USA',
+            source: 'Wikipedia',
+            link: 'https://en.wikipedia.org/wiki/Steve_McCurry',
+            years: '1950-present',
+            description: 'Steve McCurry became interested in photography when he started taking pictures for the Penn State newspaper The Daily Collegian. After working at Today`s Post in King of Prussia, Pennsylvania for two years, he left for India to freelance. It was here that McCurry learned to watch and wait on life. His career was launched when, disguised in native garb, he crossed the Pakistan border into rebel-controlled areas of Afghanistan just before the Soviet invasion. When he emerged, he had rolls of film sewn into his clothes. Those images, which were published around the world, were among the first to show the conflict. His coverage won the Robert Capa Gold Medal for Best Photographic Reporting from Abroad, an award dedicated to photographers exhibiting exceptional courage and enterprise. McCurry continued to cover armed conflicts, including the Iran-Iraq War, Lebanon Civil War, the Cambodian Civil War, the Islamic insurgency in the Philippines, the Gulf War and the Afghan Civil War. His work has been featured worldwide in magazines and he is a frequent contributor to National Geographic. He has been a member of Magnum Photos since 1986. He is the recipient of numerous awards, including Magazine Photographer of the Year, awarded by the National Press Photographers Association. The same year, he won an unprecedented four first-place prizes in the World Press Photo contest. McCurry focuses on the human consequences of war, not only showing what war impresses on the landscape, but rather, on the human face. McCurry took his most recognized portrait, "Afghan Girl", in December 1984 of an approximately 12-year-old Pashtun orphan in the Nasir Bagh refugee camp near Peshawar, Pakistan. McCurry found the girl when he heard "unexpected laughter" coming from children inside a one-room school tent for girls. "I noticed this one little girl with these incredible eyes, and I instantly knew that this was really the only picture I wanted to take," he says. This was the first time the girl had ever been photographed. The image itself was named as "the most recognized photograph" in the history of the National Geographic magazine, and her face became famous as the cover photograph on the June 1985 issue.',
+            autor: 'Steve McCurry',
+            quote: 'If you wait, people will forget your camera and the soul will drift up into view.',
+            mainBannerTitle: '', //перед названием нужна запятая
+            mainImage: '../assets/post2/1-afghan-girl.JPG',
+            image1: 'post2/2-old-man-portrait.jpg',
+            image2: 'post2/3-ShaolinMonksTrainingZhengzhou,china,2004.jpg',
+            image3: 'post2/4-MotherandChildatCarWindow.jpg',
+            images: [
+                'post2/1-afghan-girl.JPG',
+                'post2/2-old-man-portrait.jpg',
+                'post2/3-ShaolinMonksTrainingZhengzhou,china,2004.jpg',
+                'post2/4-MotherandChildatCarWindow.jpg',
+                'post2/Camels and Oil Fields Al Ahmadi, Kuwait, 1991.jpg',
+                'post2/Girl Prays (Afghanistan, 2003).jpg',
+                'post2/Holi Man (Rajastan, India, 1996).jpg',
+                'post2/monks-play.jpg',
+                'post2/red-boy.jpg',
+                'post2/SRILANKA.jpg',
+                'post2/Village Boy (Kamdesh, Nuristan, Afghanistan, 1992).jpg',
+                'post2/Young Monks Play Video Games (Bylakuppe, India, 2001).jpg'
+            ]
+        },
+        {
+            id: 3,
+            country: 'USA',
+            source: 'The Art Story',
+            link: 'http://www.theartstory.org/artist-avedon-richard.htm',
+            years: '1923 – 2004',
+            description: 'In a gesture of supreme, youthful confidence, Richard Avedon did away with the standard trope of statue-like, frozen-in-time models of conventional fashion photography. Instead the exuberant young photographer who legendarily never stood still, enlivened his models and, most importantly, showed their human side, flaws and all. He is probably best known, however, for his arresting, black-and-white and often large-format portraits of people, whether celebrities or unknowns, which are as much psychological studies as physical ones. Ranging between the commercial work he did as a fashion photographer and the ground-breaking fine art portraiture, the breadth and creativity of Avedon`s body of work has made him one of the most influential photographers of the 20th century. His photographs, claimed the New York Times, "helped define America`s image of style and beauty and culture" since the 1950s. Avedon`s brash, youthful, and iconoclastic entry into the genre of fashion photography signaled a point of no return: no longer were fashion models frozen in time, statue-like, and imperturbable. Rather, they were living, moving, imperfect beings however beautiful. He imposed simple narratives so that the viewers and consumers could invest as much in a story about a garment (and its model) as the garment itself. His spare portraits, most often in black-and-white, large format, and often blurred in portions, explored a diverse range of themes such as sexuality, violence, and death. The images, whether of celebrities or world leaders or eccentrics were as much about his sitters` interior lives as hinted at by their external appearances.',
+            autor: 'Richard Avedon',
+            quote: 'Sometimes I think all my pictures are just pictures of me.',
+            mainBannerTitle: '', //перед названием нужна запятая
+            mainImage: '../assets/post3/1-RichardAvedon,Dovimawithelephants,eveningdressbyDior,Cirqued’Hiver,Paris,August1955_©2009TheRichardAvedonFoundation.jpg',
+            image1: 'post3/2-KatharineHepburn,actor,NewYork,March2,1955.jpeg',
+            image2: 'post3/3-AudreyHepburnNewYork,January1967.jpg',
+            image3: 'post3/4-BarbaraStreisand,singer,NewYork,October1,1965.jpeg',
+            images: [
+                'post3/1-RichardAvedon,Dovimawithelephants,eveningdressbyDior,Cirqued’Hiver,Paris,August1955_©2009TheRichardAvedonFoundation.jpg',
+                'post3/2-KatharineHepburn,actor,NewYork,March2,1955.jpeg',
+                'post3/3-AudreyHepburnNewYork,January1967.jpg',
+                'post3/4-BarbaraStreisand,singer,NewYork,October1,1965.jpeg',
+                'post3/Antonella Agnelli Hair by Kenneth, New York, January 1961.jpg',
+                'post3/Elizabeth Taylor, actor, New York, July 1, 1964.jpg',
+                'post3/Juan Patricio Lobado, Carney, Rocky Ford, Colorado, August 23, 1980.jpg',
+                'post3/Marian Anderson, singer, 1955, Gelatin silver print.jpg',
+                'post3/Ronald Fischer, Beekeeper, Davis, California, May 9, 1981.jpg',
+                'post3/Stephanie Seymour  Dress and shoes by Chanel, Paris, September 1994.jpg',
+                'post3/twiggy1.jpg',
+                'post3/Veruschka Dress by Kimberly, New York, January 1967.jpg'
+            ]
+        },
+        {
+            id: 4,
+            country: 'USA',
+            source: 'Gapyear',
+            link: 'https://www.gapyear.com/articles/features/ansel-adams-25-beautifully-nostalgic-photos',
+            years: '1902-1984',
+            description: 'Ansel Adams was one of the greatest landscape photographers of all time. As a young child he had aspirations of of becoming a concert pianist, but soon set his sights on photography after a family holiday to Yosemite national park in 1916. Working as caretaker and a photo technician at the Sierra Club in Yosemite Valley, he soon accomplished his dream of becoming a full-time photographer, publishing his first prints of the High Sierras in 1927, and of Taos Pueblo in 1930. While other photographers concentrated on photojournalism, Adams was the first person to truly pioneer photography as an artform. Shooting almost exclusively in black and white, he worked primarily with large-format cameras to produce stunning high-resolution landscapes and pin-sharp textures. Adams travelled extensively with his camera, throughout a career spanning over 8 decades, but it was that first Yosemite trip that really stole his heart. Returning a great many times throughout his life, he visited the birthplace of his artform for the last time in 1983, before passing away aged 82, in 1984. Below we highlight the most beautiful works of Ansel Adams’ extensive career. Click on the images to view large.',
+            autor: 'Ansel Adams',
+            quote: 'You don’t take a photograph, you make it.',
+            mainBannerTitle: '', //перед названием нужна запятая
+            mainImage: '../assets/post4/1-tree.jpg',
+            image1: 'post4/2-church.jpg',
+            image2: 'post4/3-TheTetonsandtheSnakeRiver.jpg',
+            image3: 'post4/4-bare-trees.jpg',
+            images: [
+                'post4/1-tree.jpg',
+                'post4/2-church.jpg',
+                'post4/3-TheTetonsandtheSnakeRiver.jpg',
+                'post4/4-bare-trees.jpg',
+                'post4/alley.jpg',
+                'post4/ansel-adams-clearing-winter-storm.jpg',
+                'post4/Church, Taos Pueblo (1942).jpg',
+                'post4/garnet-lake-thunderclouds.jpg',
+                'post4/lake.jpeg',
+                'post4/McDonald Lake, Glacier National Park (1942).jpg',
+                'post4/Mt. Moran, Teton National Park.jpg',
+                'post4/Navajo Woman and Infant.jpg',
+                'post4/Portrait of Tom Kobayashi at Manzanar, 1943.jpg'
             ]
         }
     ],
