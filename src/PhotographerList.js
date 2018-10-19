4973d7717f834b2049ad7d1f533df09cedfca75d
HAULMONT\laptev <laptev@haulmont.com>
2018-10-19 10:20:42 +0400

    Fixed some bugs.

--- src/PhotographerList.js
+++ src/PhotographerList.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import posts from './posts';
-import ListItem from './ListItem';
-import MainBanner from './MainBanner';
-import ButtonToTop from './ButtonToTop';
-import Footer from './Footer';
-import './PhotographerList.css';
+import React, { Component } from "react";
+import posts from "./posts";
+import ListItem from "./ListItem";
+import MainBanner from "./MainBanner";
+import ButtonToTop from "./ButtonToTop";
+import Footer from "./Footer";
+import "./PhotographerList.css";
 import { Helmet } from "react-helmet";
 
 class PhotographerList extends Component {
@@ -16,30 +16,29 @@ class PhotographerList extends Component {
 
   // показываем еще 10 постов
   showMore() {
-    this.setState({ numItems: this.state.numItems + 10 })
+    this.setState({ numItems: this.state.numItems + 10 });
   }
 
-
   render() {
-
     // записываем в переменную 10 постов в обратном порядке
     var reversePosts = posts.getReverseArray(this.state.numItems);
 
     // выводим на страницу 10 постов
     var post = reversePosts.map((item, index) => {
-      return <ListItem
-        key={index}
-        path={'/detail/' + item.id}
-        item={item} />
-    })
+      return <ListItem key={index} path={"/detail/" + item.id} item={item} />;
+    });
 
     // кнопка показать больше выводится на странице пока есть непоказанные посты
     // в противном случае не кнопка не выводится на странице
     let buttonShowMore;
     if (this.state.numItems < posts.data.length) {
-      buttonShowMore = <div className='show-more'>
-        <button onClick={this.showMore.bind(this)}>SHOW 10 PREVIOUS GALLERIES</button>
-      </div>;
+      buttonShowMore = (
+        <div className="show-more">
+          <button onClick={this.showMore.bind(this)}>
+            SHOW 10 PREVIOUS GALLERIES
+          </button>
+        </div>
+      );
     }
 
     return (
@@ -47,11 +46,11 @@ class PhotographerList extends Component {
         {/* this code for document.head */}
         <Helmet>
           <title>Legendary Photographers</title>
-          <meta name='description' content='The best photos in the world' />
+          <meta name="description" content="The best photos in the world" />
         </Helmet>
         {/* this code for document.head */}
         <MainBanner />
-        {post}
+        <div className="list-of-photo-container">{post}</div>
         {buttonShowMore}
         <ButtonToTop />
         <Footer />
